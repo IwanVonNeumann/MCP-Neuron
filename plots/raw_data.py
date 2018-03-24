0c0aaa4d9776de2f0c95c0e3867adb305f8a6651
IwanVonNeumann <grabageheap@inbox.lv>
2018-03-24 10:33:35 +0200

    raw data plots refactored

--- plots/raw_data.py
+++ plots/raw_data.py
@@ -1,16 +1,12 @@
 import matplotlib.pyplot as plt
 
-from data_access.iris import load_iris_data
-
 
 # y = df.iloc[0:100, 4].values
 # y = np.where(y == "Iris-setosa", -1, 1)
 
-def two_classes_plot():
-    iris_df = load_iris_data()
-
-    set_df = iris_df.loc[iris_df["class"] == "Iris-setosa"]
-    ver_df = iris_df.loc[iris_df["class"] == "Iris-versicolor"]
+def two_classes_plot(iris_df):
+    set_df = iris_df[iris_df["class"] == "Iris-setosa"]
+    ver_df = iris_df[iris_df["class"] == "Iris-versicolor"]
 
     plt.scatter(set_df["sepal length"], set_df["petal length"], color="red", marker="o", label="setosa")
     plt.scatter(ver_df["sepal length"], ver_df["petal length"], color="blue", marker="x", label="versicolor")
@@ -20,12 +16,10 @@ def two_classes_plot():
     plt.show()
 
 
-def three_classes_plot():
-    iris_df = load_iris_data()
-
-    set_df = iris_df.loc[iris_df["class"] == "Iris-setosa"]
-    ver_df = iris_df.loc[iris_df["class"] == "Iris-versicolor"]
-    vir_df = iris_df.loc[iris_df["class"] == "Iris-virginica"]
+def three_classes_plot(iris_df):
+    set_df = iris_df[iris_df["class"] == "Iris-setosa"]
+    ver_df = iris_df[iris_df["class"] == "Iris-versicolor"]
+    vir_df = iris_df[iris_df["class"] == "Iris-virginica"]
 
     plt.scatter(set_df["sepal length"], set_df["petal length"], color="blue", marker="x", label="setosa")
     plt.scatter(ver_df["sepal length"], ver_df["petal length"], color="red", marker="o", label="versicolor")
--- raw_data_plots.py
+++ raw_data_plots.py
@@ -1,4 +1,7 @@
+from data_access.iris import load_iris_data
 from plots.raw_data import two_classes_plot, three_classes_plot
 
-# two_classes_plot()
-three_classes_plot()
+iris_df = load_iris_data()
+
+# two_classes_plot(iris_df)
+three_classes_plot(iris_df)
