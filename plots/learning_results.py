e48dcf01005b374dec236d09e0c27dd5060dbdfd
IwanVonNeumann <grabageheap@inbox.lv>
2018-03-24 21:30:07 +0200

    decision boundary plot drawing refactored

--- plots/learning_results.py
+++ plots/learning_results.py
@@ -14,22 +14,13 @@ def plot_2d_decision_boundary(iris_df, classifier):
     set_df = iris_df[iris_df["class"] == "Iris-setosa"]
     ver_df = iris_df[iris_df["class"] == "Iris-versicolor"]
 
-    min_x = min(set_df["sepal length"].min(), ver_df["sepal length"].min())
-    max_x = max(set_df["sepal length"].max(), ver_df["sepal length"].max())
-
-    min_y = min(set_df["petal length"].min(), ver_df["petal length"].min())
-    max_y = max(set_df["petal length"].max(), ver_df["petal length"].max())
-
-    w = classifier.w_
-    des_y1 = - (w[0] + w[1] * min_x) / w[2]
-    des_y2 = - (w[0] + w[1] * max_x) / w[2]
-
-    des_x = [min_x - 0.5, max_x + 0.5]
+    min_x, max_x, min_y, max_y, des_y1, des_y2 = decision_boundary(iris_df, classifier)
+    des_x = [min_x, max_x]
     des_y = [des_y1, des_y2]
 
     plt.plot(des_x, des_y, color="navy", linewidth=2)
-    plt.fill_between(des_x, min_y - 0.5, des_y, color="blue", alpha=0.25)
-    plt.fill_between(des_x, des_y, max_y + 0.5, color="red", alpha=0.25)
+    plt.fill_between(des_x, min_y, des_y, color="blue", alpha=0.25)
+    plt.fill_between(des_x, des_y, max_y, color="red", alpha=0.25)
 
     plt.scatter(set_df["sepal length"], set_df["petal length"], color="blue", marker="x", label="setosa")
     plt.scatter(ver_df["sepal length"], ver_df["petal length"], color="red", marker="o", label="versicolor")
@@ -39,3 +30,22 @@ def plot_2d_decision_boundary(iris_df, classifier):
     plt.ylabel("petal length")
     plt.legend(loc="upper left")
     plt.show()
+
+
+def decision_boundary(iris_df, classifier):
+    setosa_versicolor = ["Iris-setosa", "Iris-versicolor"]
+    set_ver_df = iris_df[iris_df["class"].isin(setosa_versicolor)]
+
+    b = 0.5
+
+    min_x = set_ver_df["sepal length"].min() - b
+    max_x = set_ver_df["sepal length"].max() + b
+
+    min_y = set_ver_df["petal length"].min() - b
+    max_y = set_ver_df["petal length"].max() + b
+
+    w = classifier.w_
+    des_y1 = - (w[0] + w[1] * min_x) / w[2]
+    des_y2 = - (w[0] + w[1] * max_x) / w[2]
+
+    return min_x, max_x, min_y, max_y, des_y1, des_y2
