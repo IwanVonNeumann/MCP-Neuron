a6f03d6ec6af9cb3e3105c5bb3e2377f715d9388
Iwan Ruskiewicz <ivans.ruskevics@ideaportriga.lv>
2018-03-27 18:00:41 +0300

    Adaline usage example drafts added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 from data_access.iris import load_iris_data
+from neurons.adaline import Adaline
 from neurons.perceptron import Perceptron
-from plots.learning_results import plot_2d_decision_boundary
+from plots.learning_results import plot_2d_decision_boundary, plot_error_history
 
 iris_df = load_iris_data()
 
@@ -23,9 +27,40 @@ predictor_columns = ["sepal length", "petal length"]
 train_X = set_ver_df[predictor_columns].values
 train_y = set_ver_df["binary class"].values
 
-perceptron = Perceptron(eta=0.1, n_iter=10)
-perceptron.fit(train_X, train_y)
+# perceptron = Perceptron(eta=0.1, n_iter=10)
+# perceptron.fit(train_X, train_y)
 
 # plot_error_history(perceptron.errors_history_)
 
-plot_2d_decision_boundary(iris_df, perceptron)
+# plot_2d_decision_boundary(iris_df, perceptron)
+
+# adaline = Adaline(eta=0.1, n_iter=10)
+# adaline.fit(train_X, train_y)
+
+# plot_error_history(adaline.cost_history_)
+
+# plot_2d_decision_boundary(iris_df, adaline)
+
+ada_1 = Adaline(n_iter=10, eta=0.01)
+ada_1.fit(train_X, train_y)
+
+for i, x in enumerate(ada_1.cost_history_):
+    print(i, x)
+
+ada_2 = Adaline(n_iter=10, eta=0.0001)
+ada_2.fit(train_X, train_y)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+
+ax[0].plot(range(1, len(ada_1.cost_history_) + 1), np.log10(ada_1.cost_history_), marker='o')
+# ax[0].plot(range(1, len(ada1.cost_history_) + 1), ada1.cost_history_, marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ax[1].plot(range(1, len(ada_2.cost_history_) + 1), ada_2.cost_history_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+plt.show()
--- neurons/adaline.py
+++ neurons/adaline.py
@@ -36,14 +36,14 @@ class Adaline(object):
         self : object
         """
         self.w_ = np.zeros(1 + X.shape[1])
-        self.cost_ = []
+        self.cost_history_ = []
         for i in range(self.n_iter):
             output = self.net_input(X)
             errors = (y - output)
             self.w_[1:] += self.eta * X.T.dot(errors)
             self.w_[0] += self.eta * errors.sum()
             cost = (errors ** 2).sum() / 2.0
-            self.cost_.append(cost)
+            self.cost_history_.append(cost)
         return self
 
     def net_input(self, X):
