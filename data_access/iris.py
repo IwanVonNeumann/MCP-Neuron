d359709d8d4bd0fdd3672c880d1030fe33120d78
IwanVonNeumann <grabageheap@inbox.lv>
2018-04-27 18:02:57 +0300

    feature names added to dataset file

--- data_access/iris.py
+++ data_access/iris.py
@@ -7,15 +7,4 @@ data_file_path = os.path.join(data_dir, data_file_name)
 
 
 def load_iris_data():
-    columns = [
-        "sepal length",
-        "sepal width",
-        "petal length",
-        "petal width",
-        "class"
-    ]
-
-    iris_df = pd.read_csv(data_file_path, header=None)
-    iris_df.columns = columns
-
-    return iris_df
+    return pd.read_csv(data_file_path)
