42dad1cdbed58a5700a008e50b5e51c7bc0eeecf
IwanVonNeumann <grabageheap@inbox.lv>
2019-01-16 22:02:04 +0200

    docs removed from Adaline class

--- neurons/adaline.py
+++ neurons/adaline.py
@@ -1,41 +1,13 @@
 import numpy as np
 
 
-class Adaline(object):
-    """ADAptive LInear NEuron classifier.
-    Parameters
-    ------------
-    learning_rate : float
-    Learning rate (between 0.0 and 1.0)
-    n_iter : int
-    Passes over the training dataset.
-    Attributes
-    -----------
-    w_ : 1d-array
-    Weights after fitting.
-    cost_history_ : list
-    Number of misclassifications in every epoch.
-    """
+class Adaline:
 
     def __init__(self, learning_rate=0.01, n_iter=50):
         self.learning_rate = learning_rate
         self.n_iter = n_iter
 
     def fit(self, X, y):
-        """ Fit training data.
-        Parameters
-        ----------
-        X : {array-like}, shape = [n_samples, n_features]
-        Training vectors,
-        where n_samples is the number of samples and
-        n_features is the number of features.
-        y : array-like, shape = [n_samples]
-        Target values.
-        Returns
-        -------
-        self : object
-        """
-
         n, m = X.shape
 
         self.w_ = np.zeros(m)
@@ -53,13 +25,10 @@ class Adaline(object):
         return self
 
     def net_input(self, X):
-        """Calculate net input"""
         return X.dot(self.w_) + self.b_
 
     def activation(self, X):
-        """Compute linear activation"""
         return self.net_input(X)
 
     def predict(self, X):
-        """Return class label after unit step"""
         return np.where(self.activation(X) >= 0.0, 1, -1)
